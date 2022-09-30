397b1aeed4d543624f1efec7147ac47b73074df4
Nikita Kuritsyn <nyan18032003@gmail.com>
2022-09-30 10:07:30 +0300

    bugfix

--- DB_Lab1/src/main/java/src/domains/StudentsVariant.java
+++ DB_Lab1/src/main/java/src/domains/StudentsVariant.java
@@ -4,10 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serial;
+import java.io.Serializable;
 
 @Getter
 @Setter
-public class StudentsVariant {
+public class StudentsVariant implements Serializable {
     @Serial
     private static final long serialVersionUID = 899L;
     private Student student;
